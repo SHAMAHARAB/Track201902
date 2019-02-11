f8ba05f0c52f741d56a14dbba146883436bc9652
浜原広季 <hiroki.hamahara@g.softbank.co.jp>
2019-02-11 23:03:21 +0900

    【初期コミット】#1 プロジェクト作成 https://github.com/SHAMAHARAB/Track201902/projects/1#card-17509149

---
+++ src/main/java/jp/co/softbank/trackproject/TrackprojectApplication.java
@@ -0,0 +1,14 @@
+package jp.co.softbank.trackproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackprojectApplication.class, args);
+	}
+
+}
+
