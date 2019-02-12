e1c71359406d9d4b0a9968d4ef9217871df41b45
浜原広季 <hiroki.hamahara@g.softbank.co.jp>
2019-02-13 00:57:14 +0900

    【wip】テストクラスの作成

---
+++ src/test/java/jp/co/softbank/trackproject/service/RecipeServiceImplTest.java
@@ -0,0 +1,14 @@
+package jp.co.softbank.trackproject.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RecipeServiceImplTest {
+
+  @Test
+  public void test_create() {
+    fail("Not yet implemented");
+  }
+
+}
