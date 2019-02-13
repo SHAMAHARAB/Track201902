6bcfc5ef1c454a35cfd1455f031d724c39a206d3
浜原広季 <hiroki.hamahara@g.softbank.co.jp>
2019-02-14 03:05:12 +0900

    【wip】DB

--- src/test/java/jp/co/softbank/trackproject/repository/mybatis/RecipeRepositoryMapperTest.java
+++ src/test/java/jp/co/softbank/trackproject/repository/mybatis/RecipeRepositoryMapperTest.java
@@ -1,7 +1,17 @@
 package jp.co.softbank.trackproject.repository.mybatis;
 
-import jp.co.softbank.trackproject.model.Recipe;
+import java.io.File;
+import java.net.MalformedURLException;
+import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
+import org.dbunit.Assertion;
+import org.dbunit.DatabaseUnitException;
+import org.dbunit.database.DatabaseConnection;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.ITable;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
@@ -14,12 +24,18 @@ import org.springframework.test.context.support.DependencyInjectionTestExecution
 import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+
+import jp.co.softbank.trackproject.model.Recipe;
+import jp.co.softbank.trackproject.tool.ResourceLoadHelper;
+
 @MybatisTest
 @ContextConfiguration
 @TestExecutionListeners({
     DependencyInjectionTestExecutionListener.class,
     DirtiesContextTestExecutionListener.class,
-    TransactionalTestExecutionListener.class
+    TransactionalTestExecutionListener.class,
+    DbUnitTestExecutionListener.class
 })
 @RunWith(SpringRunner.class)
 @TestPropertySource(
@@ -28,11 +44,24 @@ public class RecipeRepositoryMapperTest {
   
   @Autowired
   private RecipeRepositoryMapper target;
+  
+  @Autowired
+  private DataSource dataSourceTest;
 
   @Test
-  public void test() {
+  public void test() throws SQLException, DatabaseUnitException, MalformedURLException {
     Recipe recipe = new Recipe("トマトスープ", "15分", "5人", "玉ねぎ, トマト, スパイス, 水", 450);
     target.insert(recipe);
+    
+    DatabaseConnection connection = 
+        new DatabaseConnection((this.dataSourceTest).getConnection());
+    IDataSet databaseDataSet = connection.createDataSet();
+    ITable actualTable = databaseDataSet.getTable("recipes");
+    
+    IDataSet expectedDataSet = new FlatXmlDataSetBuilder().build(new File("create-test.xml"));
+    ITable expectedTable = expectedDataSet.getTable("recipes");
+    
+    Assertion.assertEquals(expectedTable, actualTable);
   }
 
 }
