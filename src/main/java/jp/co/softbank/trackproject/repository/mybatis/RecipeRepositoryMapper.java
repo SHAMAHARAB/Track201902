88e3ec23a1c4eee471832ded63d53d6bc998eb09
浜原広季 <hiroki.hamahara@g.softbank.co.jp>
2019-02-14 01:02:59 +0900

    【wip】テスト対象のRepositoryに@Mapperを付与し、テストクラスに必要なアノテーションを付与し、DIで取得可能にする。

--- src/main/java/jp/co/softbank/trackproject/repository/mybatis/RecipeRepositoryMapper.java
+++ src/main/java/jp/co/softbank/trackproject/repository/mybatis/RecipeRepositoryMapper.java
@@ -2,6 +2,9 @@ package jp.co.softbank.trackproject.repository.mybatis;
 
 import jp.co.softbank.trackproject.repository.RecipeRepository;
 
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface RecipeRepositoryMapper extends RecipeRepository {
   
 }
--- src/test/java/jp/co/softbank/trackproject/repository/mybatis/RecipeRepositoryMapperTest.java
+++ src/test/java/jp/co/softbank/trackproject/repository/mybatis/RecipeRepositoryMapperTest.java
@@ -1,14 +1,36 @@
 package jp.co.softbank.trackproject.repository.mybatis;
 
-import static org.junit.Assert.*;
+import jp.co.softbank.trackproject.model.Recipe;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 
+@MybatisTest
+@ContextConfiguration
+@TestExecutionListeners({
+    DependencyInjectionTestExecutionListener.class,
+    DirtiesContextTestExecutionListener.class,
+    TransactionalTestExecutionListener.class
+})
+@RunWith(SpringRunner.class)
+@TestPropertySource(
+    properties = "spring.test.database.replace: none")
 public class RecipeRepositoryMapperTest {
+  
+  @Autowired
+  private RecipeRepositoryMapper target;
 
   @Test
   public void test() {
-    fail("Not yet implemented");
   }
 
 }
