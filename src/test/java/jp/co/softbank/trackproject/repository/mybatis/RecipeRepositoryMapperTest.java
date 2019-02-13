bae035f74804072f20667169835ee82e93fbc965
浜原広季 <hiroki.hamahara@g.softbank.co.jp>
2019-02-14 00:45:50 +0900

    【wip】テストクラスの作成

---
+++ src/test/java/jp/co/softbank/trackproject/repository/mybatis/RecipeRepositoryMapperTest.java
@@ -0,0 +1,14 @@
+package jp.co.softbank.trackproject.repository.mybatis;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RecipeRepositoryMapperTest {
+
+  @Test
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}
