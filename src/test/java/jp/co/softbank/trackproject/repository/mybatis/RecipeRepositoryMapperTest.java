47f43e0d9e98440bf9aa4a44537576032129bea3
浜原広季 <hiroki.hamahara@g.softbank.co.jp>
2019-02-15 15:10:58 +0900

    【wip】dbUnitでxml形式のRecipeデータを読み取る

--- src/test/java/jp/co/softbank/trackproject/repository/mybatis/RecipeRepositoryMapperTest.java
+++ src/test/java/jp/co/softbank/trackproject/repository/mybatis/RecipeRepositoryMapperTest.java
@@ -1,10 +1,13 @@
 package jp.co.softbank.trackproject.repository.mybatis;
 
-import java.io.File;
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+
 import java.net.MalformedURLException;
 import java.sql.SQLException;
 
 import javax.sql.DataSource;
+
+import jp.co.softbank.trackproject.model.Recipe;
 
 import org.dbunit.Assertion;
 import org.dbunit.DatabaseUnitException;
@@ -23,11 +26,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
-
-import com.github.springtestdbunit.DbUnitTestExecutionListener;
-
-import jp.co.softbank.trackproject.model.Recipe;
-import jp.co.softbank.trackproject.tool.ResourceLoadHelper;
 
 @MybatisTest
 @ContextConfiguration
@@ -58,7 +56,8 @@ public class RecipeRepositoryMapperTest {
     IDataSet databaseDataSet = connection.createDataSet();
     ITable actualTable = databaseDataSet.getTable("recipes");
     
-    IDataSet expectedDataSet = new FlatXmlDataSetBuilder().build(new File("create-test.xml"));
+    IDataSet expectedDataSet = new FlatXmlDataSetBuilder()
+        .build(RecipeRepositoryMapperTest.class.getResourceAsStream("create-test.xml"));
     ITable expectedTable = expectedDataSet.getTable("recipes");
     
     Assertion.assertEquals(expectedTable, actualTable);
