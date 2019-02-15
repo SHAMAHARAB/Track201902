009beac721100356fea7fd9c10588c9474e7eafa
浜原広季 <hiroki.hamahara@g.softbank.co.jp>
2019-02-15 15:45:38 +0900

    【wip】テスト対象クラス、メソッドを作成

---
+++ src/main/java/jp/co/softbank/trackproject/controller/RecipeController.java
@@ -0,0 +1,14 @@
+package jp.co.softbank.trackproject.controller;
+
+import jp.co.softbank.trackproject.model.Recipe;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RecipeController {
+
+  public Recipe create() {
+    return null;
+  }
+
+}
