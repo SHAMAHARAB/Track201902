ea8ad744f6c622f16b5c3df174cc282f364c496b
浜原広季 <hiroki.hamahara@g.softbank.co.jp>
2019-02-15 16:06:49 +0900

    【wip】mockMvcで指定したURLを叩けているか確認

--- src/main/java/jp/co/softbank/trackproject/controller/RecipeController.java
+++ src/main/java/jp/co/softbank/trackproject/controller/RecipeController.java
@@ -3,11 +3,15 @@ package jp.co.softbank.trackproject.controller;
 import jp.co.softbank.trackproject.client.response.RecipeResponse;
 import jp.co.softbank.trackproject.model.Recipe;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/recipes")
 public class RecipeController {
 
+  @PostMapping
   public RecipeResponse create() {
     return new RecipeResponse(new Recipe("トマトスープ", "15分", "5人", "玉ねぎ, トマト, スパイス, 水", 450));
   }
--- src/test/java/jp/co/softbank/trackproject/controller/RecipeControllerTest.java
+++ src/test/java/jp/co/softbank/trackproject/controller/RecipeControllerTest.java
@@ -1,23 +1,24 @@
 package jp.co.softbank.trackproject.controller;
 
-import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebMvcTest(RecipeControllerTest.class)
 public class RecipeControllerTest {
   
   @Autowired
-  private RecipeController target;
+  private MockMvc mockMvc;
 
   @Test
-  public void test_create() {
-    target.create();
+  public void test_create() throws Exception {
+    mockMvc.perform(post("/recipes"));
   }
 
 }
